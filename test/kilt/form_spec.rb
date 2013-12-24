950ffec138de38e5849da0db714c384a15e49780
Darren Cauthon <darren@cauthon.com>
2013-12-23 20:47:44 -0600

    Test when the named view fails.

--- test/kilt/form_spec.rb
+++ test/kilt/form_spec.rb
@@ -26,7 +26,48 @@ describe Kilt::Form do
                                    } )
                      .returns rendered_action_view
 
-          ActionView::Base.expects(:new)
+          ActionView::Base.stubs(:new)
+                          .with(Kilt::Form::TEMPLATES_DIR)
+                          .returns action_view
+
+          result = Kilt::Form.send(method.to_sym, object, field_name, index)
+          
+          result.must_be_same_as rendered_action_view
+
+        end
+
+      end
+
+      describe "when the view does not exist" do
+
+        it "should return the default view" do
+
+          rendered_action_view = Object.new
+
+          object     = Object.new
+          field_name = Object.new
+          index      = Object.new
+
+          action_view = Object.new
+          action_view.expects(:render)
+                     .with(file: "#{method}.html.erb", 
+                           locals: { 
+                                     object:     object, 
+                                     field_name: field_name,
+                                     index:      index
+                                   } )
+                     .raises 'error'
+
+          action_view.expects(:render)
+                     .with(file: "_default.html.erb", 
+                           locals: { 
+                                     object:     object, 
+                                     field_name: field_name,
+                                     index:      index
+                                   } )
+                     .returns rendered_action_view
+
+          ActionView::Base.stubs(:new)
                           .with(Kilt::Form::TEMPLATES_DIR)
                           .returns action_view
 
