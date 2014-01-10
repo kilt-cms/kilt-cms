1811685807f783fbf05eeec5b62c3d00cb98b03a
Tim Boisvert <tboisvert@asheavenue.com>
2014-01-10 14:04:19 -0500

    added Select_one feature

--- lib/kilt/form.rb
+++ lib/kilt/form.rb
@@ -8,17 +8,38 @@ module Kilt
                     end
     
     def self.method_missing(method, *args)
-      locals = { object: args[0], field_name: args[1], index: args[2] }
+      locals = { object: args[0], field_name: args[1], index: args[2], options: args[3] }
       render_view method, locals
     rescue
       render_view '_default', locals
+    end
+    
+    def self.prep_field(method, *args)
+      # This is a method called before the generic method_missing.
+      # In this method, we'll parse out the method value to see if there are
+      # any options being passed in with the name of the field.
+      # Example: select(small|large)
+      if (method.include? "(") && (method.include? ")")
+        first_split = method.split("(")
+        method_name = first_split.first
+        
+        # Now get the option values and push them onto the arg stack
+        second_split = first_split[1].split(")")
+        options = second_split.first
+        if options
+          args << options
+        end
+      else
+        method_name = method
+      end
+      self.send(method_name.strip, *args)
     end
 
     private
 
     def self.render_view name, locals
-      data = { file:   "#{name.to_s}.html.erb",
-               locals: locals }
+      data = { :file    =>  "#{name.to_s}.html.erb",
+               :locals  => locals }
       ActionView::Base.new(TEMPLATES_DIR).render data
     end
     
