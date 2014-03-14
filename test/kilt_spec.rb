2623178d30bfe946635faefabfa97bd0a65d3dd7
Darren Cauthon <darren@cauthon.com>
2014-03-13 23:14:37 -0500

    Reorganize the tests to write simpler tests on create later.

--- test/kilt_spec.rb
+++ test/kilt_spec.rb
@@ -22,80 +22,82 @@ describe Kilt do
 
   end
 
-  [
-    ['1/1/2014', 'cat', 'Apple',  '1388556000000', 'apple' ],
-    ['2/2/2016', 'dog', 'Orange', '1454392800000', 'orange'],
-  ].map do |values|
-    Struct.new(:today, :type, :name, :unique_id, :slug).new *values
-  end.each do |scenario|
+  describe "creating an object" do
 
-    describe "creating an object" do
+    [
+      ['1/1/2014', 'cat', 'Apple',  '1388556000000', 'apple' ],
+      ['2/2/2016', 'dog', 'Orange', '1454392800000', 'orange'],
+    ].map do |values|
+      Struct.new(:today, :type, :name, :unique_id, :slug).new *values
+    end.each do |scenario|
 
-      let(:values) do
-        {}
-      end
+      describe "basic scenarios" do
 
-      let(:object) do
-        Kilt::Object.new(scenario.type, values)
-      end
+        let(:values) { {} }
 
-      before do
-        Timecop.freeze Time.parse(scenario.today)
-        values['name'] = scenario.name
-      end
+        let(:object) do
+          Kilt::Object.new(scenario.type, values)
+        end
 
-      it "should set a unique id based on time" do
-        Kilt.create(object)
-        object['unique_id'].must_equal scenario.unique_id
-      end
+        before do
+          Timecop.freeze Time.parse(scenario.today)
+          values['name'] = scenario.name
+        end
 
-      it "should set the name" do
-        Kilt.create(object)
-        object['name'].must_equal scenario.name
-      end
+        it "should set a unique id based on time" do
+          Kilt.create(object)
+          object['unique_id'].must_equal scenario.unique_id
+        end
 
-      it "should create an object that can be retrieved out of the database again" do
-        Kilt.create(object)
-        fresh = Kilt.get(object['slug'])
-        fresh['unique_id'].must_equal object['unique_id']
-      end
+        it "should set the name" do
+          Kilt.create(object)
+          object['name'].must_equal scenario.name
+        end
 
-      it "should set the type of the object" do
-        Kilt.create(object)
-        object['type'].must_equal scenario.type
-        Kilt.get(object['slug'])['type'].must_equal scenario.type
-      end
+        it "should create an object that can be retrieved out of the database again" do
+          Kilt.create(object)
+          fresh = Kilt.get(object['slug'])
+          fresh['unique_id'].must_equal object['unique_id']
+        end
 
-      it "should set the created_at" do
-        Kilt.create(object)
-        object['created_at'].must_equal Time.parse(scenario.today)
-        Kilt.get(object['slug'])['created_at'].must_equal Time.parse(scenario.today)
-      end
+        it "should set the type of the object" do
+          Kilt.create(object)
+          object['type'].must_equal scenario.type
+          Kilt.get(object['slug'])['type'].must_equal scenario.type
+        end
 
-      it "should set the updated_at" do
-        Kilt.create(object)
-        object['updated_at'].must_equal Time.parse(scenario.today)
-        Kilt.get(object['slug'])['updated_at'].must_equal Time.parse(scenario.today)
-      end
+        it "should set the created_at" do
+          Kilt.create(object)
+          object['created_at'].must_equal Time.parse(scenario.today)
+          Kilt.get(object['slug'])['created_at'].must_equal Time.parse(scenario.today)
+        end
 
-      it "should set a slug on the object" do
-        Kilt.create(object)
-        object['slug'].must_equal scenario.slug
-      end
+        it "should set the updated_at" do
+          Kilt.create(object)
+          object['updated_at'].must_equal Time.parse(scenario.today)
+          Kilt.get(object['slug'])['updated_at'].must_equal Time.parse(scenario.today)
+        end
 
-      describe "creating the record twice" do
+        it "should set a slug on the object" do
+          Kilt.create(object)
+          object['slug'].must_equal scenario.slug
+        end
 
-        it "should keep the slugs unique by appending the id to the second record" do
+        describe "creating the record twice" do
 
-          first  = Kilt::Object.new(scenario.type, values)
-          second = Kilt::Object.new(scenario.type, values.clone)
+          it "should keep the slugs unique by appending the id to the second record" do
 
-          Kilt.create(first)
-          sleep(0.05)
-          Kilt.create(second)
+            first  = Kilt::Object.new(scenario.type, values)
+            second = Kilt::Object.new(scenario.type, values.clone)
 
-          first['slug'].must_equal scenario.slug
-          second['slug'].must_equal "#{scenario.slug}-#{scenario.unique_id}"
+            Kilt.create(first)
+            sleep(0.05)
+            Kilt.create(second)
+
+            first['slug'].must_equal scenario.slug
+            second['slug'].must_equal "#{scenario.slug}-#{scenario.unique_id}"
+
+          end
 
         end
 
@@ -106,3 +108,4 @@ describe Kilt do
   end
 
 end
+
