36438a15513812cd309ad6c3bf7ede341d32a6d8
Darren Cauthon <darren@cauthon.com>
2014-03-22 00:05:01 -0500

    Wrap the generate method in unit tests.

--- test/generators/kilt/backend_generator_spec.rb
+++ test/generators/kilt/backend_generator_spec.rb
@@ -4,12 +4,6 @@ require_relative '../../../lib/generators/kilt/backend_generator'
 
 describe Kilt::Generators::BackendGenerator do
 
-  it "should exist" do
-    #raise Kilt::Generators::BackendGenerator.new.destination_root.inspect
-    #raise Kilt::Generators::BackendGenerator.new.public_methods(true).inspect
-    #raise Kilt::Generators::BackendGenerator.new.public_methods(true).inspect
-  end
-
   describe "generate" do
 
     let(:generator) { Kilt::Generators::BackendGenerator.new }
@@ -18,7 +12,7 @@ describe Kilt::Generators::BackendGenerator do
       Object.new
     end
 
-    let(:parent_name) { 'test' }
+    let(:parent_name) { 'Test' }
 
     before do
       generator.stubs(:template)
@@ -36,7 +30,31 @@ describe Kilt::Generators::BackendGenerator do
       klass.stubs(:parent_name).returns parent_name
     end
 
-    it "should be able to be called without erroring" do
+    it "should copy the default config file to the app" do
+      root_result = Object.new
+      root.stubs(:join).with('config', 'kilt', 'config.yml').returns root_result
+      generator.expects(:template).with 'config.yml.erb', root_result
+      generator.generate
+    end
+
+    it "should copy the default credential file to the app" do
+      root_result = Object.new
+      root.stubs(:join).with('config', 'kilt', 'creds.yml.example').returns root_result
+      generator.expects(:copy_file).with 'creds.yml.example', root_result
+      generator.generate
+    end
+
+    it "should copy the kilt.rb file as an app initializer" do
+      root_result = Object.new
+      root.stubs(:join).with('config', 'initializers', 'kilt.rb').returns root_result
+      generator.expects(:copy_file).with 'kilt.rb', root_result
+      generator.generate
+    end
+
+    it "should inject the Kilt routes into the app's routes" do
+      root_result = Object.new
+      root.stubs(:join).with('config', 'routes.rb').returns root_result
+      generator.expects(:inject_into_file).with root_result, "\n\tmount Kilt::Engine => '/admin', as: 'kilt_engine'\n", :after => "Test::Application.routes.draw do\n"
       generator.generate
     end
 
