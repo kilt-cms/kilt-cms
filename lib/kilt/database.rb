e69537607a3d0611f453467d7048c184e5e45f17
Darren Cauthon <darren@cauthon.com>
2014-03-23 12:57:28 -0500

    Move the database setup to the database object.

--- lib/kilt/database.rb
+++ lib/kilt/database.rb
@@ -35,6 +35,39 @@ module Kilt
       result['errors'] == 0
     end
 
+    def self.setup!
+      if Kilt.config.db.host && Kilt.config.db.port
+        begin
+          db = r.connect(:host => Kilt.config.db.host, :port => Kilt.config.db.port).repl
+        rescue
+          raise Kilt::CantConnectToDatabaseError
+        end
+
+        begin
+          #
+          # See if the db exists and create it otherwise
+          dbs = r.db_list.run
+          if !dbs.to_a.include? Kilt.config.db.db
+            r.db_create(Kilt.config.db.db).run
+          end
+          #
+          # See if the table exists and create it otherwise
+          tables = r.db(Kilt.config.db.db).table_list.run
+          if !tables.to_a.include? "objects"
+            r.db(Kilt.config.db.db).table_create("objects", :primary_key => "unique_id").run
+          end
+
+        rescue
+          raise Kilt::CantSetupDatabaseError
+        ensure
+          db.close
+        end
+
+      else
+        raise Kilt::NoDatabaseConfigError
+      end
+    end
+
     private
 
     def objects_table
