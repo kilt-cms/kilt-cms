52e24723fea1f18fc1fb1d3aa6db30439f17f0bf
Darren Cauthon <darren@cauthon.com>
2014-03-29 15:52:20 -0500

    Let's use a 4.0 generated rails app instead.

--- test/test_ar/config/initializers/session_store.rb
+++ test/test_ar/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_test_ar_session'
+TestAr::Application.config.session_store :cookie_store, key: '_test_ar_session'
--- test/test_ar/config/routes.rb
+++ test/test_ar/config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+TestAr::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
