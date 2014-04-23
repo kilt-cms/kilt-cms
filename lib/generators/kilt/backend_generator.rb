797df379d6368254af6b212db60b83e215c290fc
Darren Cauthon <darren@cauthon.com>
2014-04-23 00:27:40 -0500

    Refactor.

--- lib/generators/kilt/backend_generator.rb
+++ lib/generators/kilt/backend_generator.rb
@@ -7,16 +7,36 @@ module Kilt
       source_root File.expand_path("../templates/backend", __FILE__)
       
       def generate
-        
-        #copy files, templates for app-specific naming, inject engine mount to routes
+        add_the_config_file
+        add_the_credential_file_examples
+        add_the_database_migration_file
+        add_the_kilt_routes
+        add_the_initializer
+      end
+
+      private
+
+      def add_the_config_file
         template 'config.yml.erb', Rails.root.join('config', 'kilt', 'config.yml')
+      end
+
+      def add_the_credential_file_examples
         copy_file 'creds.yml.example', Rails.root.join('config', 'kilt', 'creds.yml.example')
         copy_file 'creds.yml.rethinkdb.example', Rails.root.join('config', 'kilt', 'creds.yml.rethinkdb.example')
-        copy_file 'create_kilt_objects.rb', Rails.root.join('db', 'migrate', Time.now.strftime("%Y%m%d%H%M%S") + "_create_kilt_objects.rb")
-        copy_file 'kilt.rb', Rails.root.join('config', 'initializers', 'kilt.rb')
-        inject_into_file Rails.root.join('config', 'routes.rb'), "\n\tmount Kilt::Engine => '/admin', as: 'kilt_engine'\n", :after => ".routes.draw do\n"
-      
       end
+
+      def add_the_database_migration_file
+        copy_file 'create_kilt_objects.rb', Rails.root.join('db', 'migrate', Time.now.strftime("%Y%m%d%H%M%S") + "_create_kilt_objects.rb")
+      end
+
+      def add_the_kilt_routes
+        inject_into_file Rails.root.join('config', 'routes.rb'), "\n\tmount Kilt::Engine => '/admin', as: 'kilt_engine'\n", :after => ".routes.draw do\n"
+      end
+
+      def add_the_initializer
+        copy_file 'kilt.rb', Rails.root.join('config', 'initializers', 'kilt.rb')
+      end
+
     end
   end
 end
