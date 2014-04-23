14aa33dac34ee36c614715c303c6d9546f29aba6
Darren Cauthon <darren@cauthon.com>
2014-04-23 00:05:55 -0500

    Make the rethinkdb creds file the exception.

--- lib/generators/kilt/backend_generator.rb
+++ lib/generators/kilt/backend_generator.rb
@@ -11,7 +11,7 @@ module Kilt
         #copy files, templates for app-specific naming, inject engine mount to routes
         template 'config.yml.erb', Rails.root.join('config', 'kilt', 'config.yml')
         copy_file 'creds.yml.example', Rails.root.join('config', 'kilt', 'creds.yml.example')
-        copy_file 'creds.yml.ar.example', Rails.root.join('config', 'kilt', 'creds.yml.ar.example')
+        copy_file 'creds.yml.rethinkdb.example', Rails.root.join('config', 'kilt', 'creds.yml.rethinkdb.example')
         copy_file 'create_kilt_objects.rb', Rails.root.join('db', 'migrate', Time.now.strftime("%Y%m%d%H%M%S") + "_create_kilt_objects.rb")
         copy_file 'kilt.rb', Rails.root.join('config', 'initializers', 'kilt.rb')
         inject_into_file Rails.root.join('config', 'routes.rb'), "\n\tmount Kilt::Engine => '/admin', as: 'kilt_engine'\n", :after => ".routes.draw do\n"
