374e56da5edd26c1c147bef79c0130831b86ee31
Darren Cauthon <darren@cauthon.com>
2014-05-17 10:53:54 -0500

    Refactor.

--- lib/kilt/formatting.rb
+++ lib/kilt/formatting.rb
@@ -1,14 +1,25 @@
 module Kilt
   module Formatting
     def self.singular_name_of object
-      type = if object.is_a? Symbol
-               object.to_s
-             elsif object.is_a? String
-               object
-             else
-               object.type.to_s
-             end
-      type.split('_').map { |x| x.capitalize }.join(' ')
+      the_type_of(object)
+        .split('_')
+        .map { |x| x.capitalize }
+        .join(' ')
+    end
+
+    class << self
+
+      private
+
+      def the_type_of object
+        if object.is_a? Symbol
+          object.to_s
+        elsif object.is_a? String
+          object
+        else
+          object.type.to_s
+        end
+      end
     end
   end
 end
