cbbfaf0d2f5de845153c71a593726fc569a57e8f
Darren Cauthon <darren@cauthon.com>
2014-05-17 10:56:33 -0500

    Refactor the tests.

--- test/kilt/formatting_spec.rb
+++ test/kilt/formatting_spec.rb
@@ -2,28 +2,28 @@ require File.expand_path(File.dirname(__FILE__) + '/../minitest_helper')
 
 describe Kilt::Formatting do
 
-  describe "singular type of" do
+  describe "getting the formatted names of things" do
 
     [
       [:horse,               'Horse'],
       [:cat,                 'Cat'],
       [:no_namey,            'No Namey'],
       [:big_green_alligator, 'Big Green Alligator'],
-    ].map { |x| Struct.new(:type, :expected).new(*x) }.each do |example|
+    ].map { |x| Struct.new(:type, :singular).new(*x) }.each do |example|
 
       describe "multiple examples" do
 
         it "should return the singular form of the kilt object" do
           object = Kilt::Object.new example.type
-          Kilt::Formatting.singular_name_of(object).must_equal example.expected
+          Kilt::Formatting.singular_name_of(object).must_equal example.singular
         end
 
         it "should return the singular form of the symbol" do
-          Kilt::Formatting.singular_name_of(example.type).must_equal example.expected
+          Kilt::Formatting.singular_name_of(example.type).must_equal example.singular
         end
 
         it "should return the singular form of the string" do
-          Kilt::Formatting.singular_name_of(example.type.to_s).must_equal example.expected
+          Kilt::Formatting.singular_name_of(example.type.to_s).must_equal example.singular
         end
 
       end
