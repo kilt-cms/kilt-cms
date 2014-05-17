d1db91aff86eb7b2f016b7f512791996ea44dbab
Darren Cauthon <darren@cauthon.com>
2014-05-17 11:01:30 -0500

    Group the like tests together.

--- test/kilt/formatting_spec.rb
+++ test/kilt/formatting_spec.rb
@@ -13,30 +13,42 @@ describe Kilt::Formatting do
 
       describe "multiple examples" do
 
-        it "should return the singular form of the kilt object" do
-          object = Kilt::Object.new example.type
-          Kilt::Formatting.singular_name_of(object).must_equal example.singular
+        describe "passing a kilt object" do
+
+          it "should return the singular form of the kilt object" do
+            object = Kilt::Object.new example.type
+            Kilt::Formatting.singular_name_of(object).must_equal example.singular
+          end
+
+          it "should return the plural form of the kilt object" do
+            object = Kilt::Object.new example.type
+            Kilt::Formatting.plural_name_of(object).must_equal example.plural
+          end
+
         end
 
-        it "should return the singular form of the symbol" do
-          Kilt::Formatting.singular_name_of(example.type).must_equal example.singular
+        describe "passing a symbol" do
+
+          it "should return the singular form of the symbol" do
+            Kilt::Formatting.singular_name_of(example.type).must_equal example.singular
+          end
+
+          it "should return the plural form of the symbol" do
+            Kilt::Formatting.plural_name_of(example.type).must_equal example.plural
+          end
+
         end
 
-        it "should return the singular form of the string" do
-          Kilt::Formatting.singular_name_of(example.type.to_s).must_equal example.singular
-        end
+        describe "passing a string" do
 
-        it "should return the plural form of the kilt object" do
-          object = Kilt::Object.new example.type
-          Kilt::Formatting.plural_name_of(object).must_equal example.plural
-        end
+          it "should return the singular form of the string" do
+            Kilt::Formatting.singular_name_of(example.type.to_s).must_equal example.singular
+          end
 
-        it "should return the plural form of the symbol" do
-          Kilt::Formatting.plural_name_of(example.type).must_equal example.plural
-        end
+          it "should return the plural form of the string" do
+            Kilt::Formatting.plural_name_of(example.type.to_s).must_equal example.plural
+          end
 
-        it "should return the plural form of the string" do
-          Kilt::Formatting.plural_name_of(example.type.to_s).must_equal example.plural
         end
 
       end
