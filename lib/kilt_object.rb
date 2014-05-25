0372c441d664e1bafe3e89fe4530f6b8bc5390ea
Darren Cauthon <darren@cauthon.com>
2014-05-25 00:01:19 -0500

    Do not include the AR-dependent class if no AR.

--- lib/kilt_object.rb
+++ lib/kilt_object.rb
@@ -1,3 +1,8 @@
-class KiltObject < ActiveRecord::Base
-  serialize :data, Hash
+# if no activerecord, then this class makes no sense
+if Object.const_defined? 'ActiveRecord'
+
+  class KiltObject < ActiveRecord::Base
+    serialize :data, Hash
+  end
+
 end
