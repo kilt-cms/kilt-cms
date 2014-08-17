ec200491329900178cbc1c1edcda008391de3b49
Darren Cauthon <darren@cauthon.com>
2014-08-17 15:16:16 -0500

    Handle this with method missing.

--- lib/kilt/upload.rb
+++ lib/kilt/upload.rb
@@ -1,14 +1,6 @@
 module Kilt
   module Upload  
     
-    def self.file(file_reference)
-      self.do('file', file_reference)
-    end
-    
-    def self.image(file_reference)
-      self.do('image', file_reference)
-    end
-    
     def self.do(type, file_reference)
       if Kilt.config.storage.strategy == 'local'
         ::Kilt::Upload::Local.upload type, file_reference
@@ -16,5 +8,12 @@ module Kilt
         ::Kilt::Upload::S3.upload type, file_reference
       end
     end
+
+    class << self
+      def method_missing(meth, *args, &blk)
+        self.do meth.to_s, args[0]
+      end
+    end
+
   end
 end
